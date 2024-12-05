e2e9f737b2413008832b07e9da321d10c065c733
ORIAMM <riovalgm.13@gmail.com>
2024-12-06 02:18:02 +0700

    Fixing Bug

---
+++ Capstonee/Assets/Script/Player TP/ToggleCam.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ToggleCam : MonoBehaviour
+{
+    private PlayerInputManager playerInputManager;
+
+    private void Awake()
+    {
+        playerInputManager = FindObjectOfType<PlayerInputManager>();
+    }
+
+    private void OnEnable()
+    {
+        playerInputManager.onPlayerJoined += ToggleThis;
+    }
+
+    private void OnDisable()
+    {
+        playerInputManager.onPlayerJoined -= ToggleThis;
+    }
+
+    private void ToggleThis(PlayerInput player)
+    {
+        this.gameObject.SetActive(false);
+    }
+
+
+}
